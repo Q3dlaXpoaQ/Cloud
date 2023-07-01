05da1ee628a9cdf59df2a1bf97a651bc5612d5a2
Cwz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2023-07-01 15:19:36 +0800

    Add files via upload

--- Simple_Cloud.js
+++ Simple_Cloud.js
@@ -2,7 +2,8 @@
     let Server;
     let messageNum = 0;
     let username = null;
-    let Message_List;
+    let Message_List='';
+    let GlobalVar='';
     class SimpleCloud {
         constructor() {
             this.isRunning = false
@@ -13,79 +14,102 @@
                 name: '简单联机',
                 color1: '#8608fc',
                 blocks: [{
-                    opcode: 'connect',
-                    blockType: 'command',
-                    text: '连接到[IP]',
-                    arguments: {
-                        IP: {
-                            type: 'string',
-                            defaultValue: 'wss://clvtpd-qnmsta-1206.preview.myide.io'
+                        opcode: 'Connect',
+                        blockType: 'command',
+                        text: '连接到[IP]',
+                        arguments: {
+                            IP: {
+                                type: 'string',
+                                defaultValue: 'wss://clvtpd-qnmsta-1206.preview.myide.io'
+                            }
                         }
-                    }
-                },
-                {
-                    opcode: 'closeSocket',
-                    blockType: 'command',
-                    text: '断开连接'
-                },
-                {
-                    opcode: 'sendMessage',
-                    blockType: 'command',
-                    text: '发送[MESSAGE]到服务器',
-                    arguments: {
-                        MESSAGE: {
-                            type: 'string',
-                            defaultValue: ''
+                    },
+                    {
+                        opcode: 'SetID',
+                        blockType: 'command',
+                        text: '将用户名设置为[ID]',
+                        arguments: {
+                            ID: {
+                                type: 'string',
+                                defaultValue: ''
+                            }
                         }
-                    }
-                },
-                {
-                    opcode: 'setID',
-                    blockType: 'command',
-                    text: '将用户名设置为[ID]',
-                    arguments: {
-                        ID: {
-                            type: 'string',
-                            defaultValue: ''
+                    },
+                    {
+                        opcode: 'CloseSocket',
+                        blockType: 'command',
+                        text: '断开连接'
+                    },
+                    {
+                        opcode: 'SendMessage',
+                        blockType: 'command',
+                        text: '发送[MESSAGE]到服务器',
+                        arguments: {
+                            MESSAGE: {
+                                type: 'string',
+                                defaultValue: ''
+                            }
                         }
-                    }
-                },
-                {
-                    opcode: 'Return_receive',
-                    blockType: 'reporter',
-                    text: '接收到的信息',
+                    },
+                    {
+                        opcode: 'SetGlobalVar',
+                        blockType: 'command',
+                        text: '将全局变量设为[Var]',
+                        arguments: {
+                            Var: {
+                                type: 'string',
+                                defaultValue: ''
+                            }
+                        }
+                    },
 
-                },
-                {
-                    opcode: 'Return_Server_List',
-                    blockType: 'reporter',
-                    text: '服务器列表',
 
-                },
+                    {
+                        opcode: 'Return_Server_List',
+                        blockType: 'reporter',
+                        text: '服务器列表',
 
-                {
-                    opcode: 'Return_username',
-                    blockType: 'reporter',
-                    text: '用户名',
+                    },
+                    {
+                        opcode: 'Return_username',
+                        blockType: 'reporter',
+                        text: '用户名',
+                    },
+                    {
+                        opcode: 'Return_receive',
+                        blockType: 'reporter',
+                        text: '接收到的信息',
 
-                },
-                {
-                    opcode: 'Return_message_Num',
-                    blockType: 'reporter',
-                    text: '共收到的数据量',
+                    },
+                    {
+                        opcode: 'Return_GVar',
+                        blockType: 'reporter',
+                        text: '全局云变量',
 
-                },
-                {
-                    opcode: 'Return_isConnect',
-                    blockType: 'Boolean',
-                    text: '是否连接到服务器?',
+                    },
+                    {
+                        opcode: 'Return_message_Num',
+                        blockType: 'reporter',
+                        text: '共收到的信息量',
 
-                },
+                    },
+                    {
+                        opcode: 'Return_Auther',
+                        blockType: 'reporter',
+                        text: '作者',
+
+                    },
+                    {
+                        opcode: 'Return_isConnect',
+                        blockType: 'Boolean',
+                        text: '是否连接到服务器?',
+
+                    },
                 ],
             };
         }
 
-        async connect({
+        async Connect({
             IP
         }) {
             const self = this;
@@ -95,22 +119,44 @@
                     self.isRunning = true
                 };
                 Server.onmessage = function (str) {
-                    Message_List = str.data
-                    messageNum++
+                    let temp_json = JSON.parse(str.data)
+                    if (temp_json instanceof Array) {
+                        Message_List = JSON.stringify(temp_json)
+                        messageNum++
+                    }
+                    else if (temp_json instanceof Object) {
+                        delete temp_json['cmd']
+                        GlobalVar = JSON.stringify(temp_json['var'])
+                    }
                 }
             }
 
         }
-        sendMessage({
+        SendMessage({
             MESSAGE
         }) {
             if (username != null) {
-                let tempjson = { 'name': username, 'val': {} }
+                let tempjson = {
+                    'name': username,
+                    'val': {}
+                }
                 tempjson['val'] = JSON.parse(MESSAGE)
                 Server.send(JSON.stringify(tempjson))
             }
         }
-        setID({
+
+        SetGlobalVar({
+            Var
+        }) {
+            if (username != null) {
+                let temp_json = {
+                    'cmd': 'GVar',
+                    'var': JSON.parse(Var)
+                }
+                Server.send(JSON.stringify(temp_json))
+            }
+        }
+        SetID({
             ID
         }) {
             username = String(ID)
@@ -122,13 +168,18 @@
         }
         Return_isConnect() {
             const self = this
-            if (Server.readyState == 1) {
+            if (Server==undefined){
+                self.isRunning=false
+            }
+            else if (Server.readyState == 1) {
                 self.isRunning = true
 
             }
             else {
                 self.isRunning = false
-                username=null
+                GlobalVar=''
+                Message_List=''
+                username = null
             }
             return String(self.isRunning)
 
@@ -139,19 +190,30 @@
         Return_username() {
             return username;
         }
-        Return_Server_List(){
-            return "{wss://clvtpd-qnmsta-1206.preview.myide.io,wss://b4b72ea4-1206-app.lightly.teamcode.com?port=1206&dcsId=b4b72ea4&token=B1zgKRFqRIW7N5ZL7FvZJA}"
+        Return_GVar() {
+            return GlobalVar;
         }
-        closeSocket() {
+        Return_Server_List() {
+            return "{wss://clvtpd-qnmsta-1206.preview.myide.io,\
+            wss://b4b72ea4-1206-app.lightly.teamcode.com?port=1206&dcsId=b4b72ea4&token=B1zgKRFqRIW7N5ZL7FvZJA}"
+        }
+        Return_Auther(){
+            return "@Q3dlaXpoaQ"
+        }
+        CloseSocket() {
             const self = this;
             if (this.isRunning) {
                 if (username != null) {
-                    Server.send(JSON.stringify({ 'name': username, 'val': 'disconnect' }))
+                    Server.send(JSON.stringify({
+                        'name': username,
+                        'val': 'disconnect'
+                    }))
                 }
-                username=null;
+                username = null;
                 Server.close();
                 self.isRunning = false;
                 Message_List = ''
+                GlobalVar=''
             }
         }
     }
